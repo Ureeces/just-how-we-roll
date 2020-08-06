fda9d78753f51f792c7af584dbbb4ebd100eef66
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-08-06 11:06:51 -0400

    Finished basic plans for dice roller and reset. Added ids for dice images.

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const twenties = [];
  * HELPER FUNCTIONS *
 ********************/
 
+// Generates a random number from 1 - max
 const getRandomNumber = function(max) {
     const rand = Math.random();
     const range = rand * max;
@@ -20,6 +21,7 @@ const getRandomNumber = function(max) {
     return result;
 }
 
+// Sorts an array in ascending order
 const sortByNumber = function(arr) {
   const byNumber = function(item1, item2) {
     return item1 - item2;
@@ -32,6 +34,37 @@ const sortByNumber = function(arr) {
  * YOUR CODE BELOW *
  *******************/
 
+// Our dice pics
+// Single d6
+const d6 = document.querySelector('#d6 roll');
+
+// Double d6
+const d6_1 = document.querySelector('#double-d6-roll-1');
+const d6_2 = document.querySelector('#double-d6-roll-1');
+
+// Single d12
+const d12 = document.querySelector('#d12-roll');
+
+// Single d20
+const d20 = document.querySelector('#d20');
+
+/*******************
+ * DICE ROLLERS *
+ *******************/
+// Rollers - These functions will:
+// 1. Calculate a random number from 1 - max
+// 2. Change the image of the dice roll 
+// 3. Add the result of the dice roll into its respective array
+
+// Universal function for die rolls, might not be used
+const roll = function(max, sides, amount) {
+  // 1. Check the amount of sides. The result decides the image directory
+  // 2a. If equal to six, we're working in the d6 image directory
+  // 2b. Otherwise, we're working in the numbers directory
+  // 3. 
+
+}
+
 
 
 /*******************
@@ -41,6 +74,13 @@ const sortByNumber = function(arr) {
 /******************
  * RESET FUNCTION *
  ******************/
+// Resets EVERYTHING
+// 1. Empties all four global arrays
+// 2. Changes the dice buttons back to their starting images
+// 3. Changes the text in our mean/median/mode text areas to nothing
+const reset = function() {
+  
+}
 
 /****************
  * MATH SECTION *
